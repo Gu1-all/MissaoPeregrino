2b53af919a952ade7e6d1ee77aa4fb98ba432dca
RaphaelRates <rafaelrats3@gmail.com>
2024-10-11 23:07:09 -0300

    fix: add menu mobile

--- js/loading.js
+++ js/loading.js
@@ -1,4 +1,6 @@
+
 const scene = new THREE.Scene();
+const element = document.querySelector("section .temp-warning");
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -23,7 +25,7 @@ function animate() {
     requestAnimationFrame(animate);
     
     // Rotação do cubo
-    cube.rotation.x += 0.01;
+    cube.rotation.x += 0.04;
     cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
